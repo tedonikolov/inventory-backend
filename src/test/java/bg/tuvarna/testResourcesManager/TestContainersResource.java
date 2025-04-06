d514854ec709550a24d56726b377dae2f847e551
tedonikolov <teonikolov01@abv.bg>
2025-04-06 14:03:54 +0300

    Fixed test containers

--- src/test/java/bg/tuvarna/testResourcesManager/TestContainersResource.java
+++ src/test/java/bg/tuvarna/testResourcesManager/TestContainersResource.java
@@ -12,6 +12,15 @@ public class TestContainersResource implements QuarkusTestResourceLifecycleManag
     private static final Network sharedNetwork = Network.newNetwork();
 
     private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("inventoryDB")
+            .withUsername("postgres")
+            .withPassword("1234")
+            .withNetwork(sharedNetwork)
+            .withNetworkAliases("postgres")
+            .withExposedPorts(5432)
+            .waitingFor(Wait.forListeningPort());
+
+    private static final PostgreSQLContainer<?> keyDB = new PostgreSQLContainer<>("postgres:latest")
             .withDatabaseName("testDB")
             .withUsername("user")
             .withPassword("password")
@@ -22,24 +31,28 @@ public class TestContainersResource implements QuarkusTestResourceLifecycleManag
     private static final GenericContainer<?> keycloak = new GenericContainer<>("quay.io/keycloak/keycloak:25.0.0")
             .withExposedPorts(8080)
             .withEnv("KC_DB", "postgres")
-            .withEnv("KC_DB_URL", "jdbc:postgresql://" + postgres.getNetworkAliases().get(0) + ":5432/testDB")
+            .withEnv("KC_DB_URL", "jdbc:postgresql://" + keyDB.getNetworkAliases().get(0) + ":5432/testDB")
             .withEnv("KC_DB_USERNAME", "user")
             .withEnv("KC_DB_PASSWORD", "password")
             .withEnv("KEYCLOAK_ADMIN", "exit")
             .withEnv("KEYCLOAK_ADMIN_PASSWORD", "tixe")
             .withCommand("start-dev")
             .withNetwork(sharedNetwork)
-            .dependsOn(postgres);
+            .dependsOn(keyDB);
 
     @Override
     public Map<String, String> start() {
         postgres.start();
+        keyDB.start();
         keycloak.start();
+
+        String dbUrl = "jdbc:postgresql://" + postgres.getHost() + ":" + postgres.getMappedPort(5432) + "/inventoryDB";
 
         String authUrl = "http://" + keycloak.getHost() + ":" + keycloak.getMappedPort(8080) + "/realms/master";
         String clientUrl = "http://" + keycloak.getHost() + ":" + keycloak.getMappedPort(8080);
 
         return Map.of(
+                "quarkus.datasource.jdbc.url", dbUrl,
                 "quarkus.oidc.auth-server-url", authUrl,
                 "quarkus.oidc.client-server-url", clientUrl
         );
