98870b1f4bff2b6d15e4f6ed6df24c8da30e4592
tedonikolov <teonikolov01@abv.bg>
2025-05-02 15:52:42 +0300

    Revert

--- src/main/java/bg/tuvarna/models/dto/CardDTO.java
+++ src/main/java/bg/tuvarna/models/dto/CardDTO.java
@@ -1,19 +1,12 @@
 package bg.tuvarna.models.dto;
 
 import bg.tuvarna.models.entities.Card;
-import jakarta.json.bind.annotation.JsonbDateFormat;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.time.LocalDate;
 
 public record CardDTO(
         Long id,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate borrowDate,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate returnDate,
         ItemDTO item,
         Long employeeId
--- src/main/java/bg/tuvarna/models/dto/ItemDTO.java
+++ src/main/java/bg/tuvarna/models/dto/ItemDTO.java
@@ -2,9 +2,6 @@ package bg.tuvarna.models.dto;
 
 import bg.tuvarna.enums.ItemStatus;
 import bg.tuvarna.enums.ItemType;
-import jakarta.json.bind.annotation.JsonbDateFormat;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.time.LocalDate;
 
@@ -16,19 +13,11 @@ public record ItemDTO(
         Double noAmortizationPrice,
         Double amortizationPrice,
         ItemType type,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate acquisitionDate,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate exploitationDate,
         ItemStatus status,
         Double amortization,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate toDate,
-        @JsonbDateFormat("dd-MM-yyyy")
-        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate deregistrationDate,
         Long categoryId,
         String imageUrl
