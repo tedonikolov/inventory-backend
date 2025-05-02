52431d6494ac3dd8aae333bde4a79b69180eea42
tedonikolov <teonikolov01@abv.bg>
2025-05-02 15:49:32 +0300

    added formated date

--- src/main/java/bg/tuvarna/models/dto/ItemDTO.java
+++ src/main/java/bg/tuvarna/models/dto/ItemDTO.java
@@ -2,6 +2,9 @@ package bg.tuvarna.models.dto;
 
 import bg.tuvarna.enums.ItemStatus;
 import bg.tuvarna.enums.ItemType;
+import jakarta.json.bind.annotation.JsonbDateFormat;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import java.time.LocalDate;
 
@@ -13,11 +16,19 @@ public record ItemDTO(
         Double noAmortizationPrice,
         Double amortizationPrice,
         ItemType type,
+        @JsonbDateFormat("dd-MM-yyyy")
+        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate acquisitionDate,
+        @JsonbDateFormat("dd-MM-yyyy")
+        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate exploitationDate,
         ItemStatus status,
         Double amortization,
+        @JsonbDateFormat("dd-MM-yyyy")
+        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate toDate,
+        @JsonbDateFormat("dd-MM-yyyy")
+        @Schema(type = SchemaType.STRING, format = "dd-MM-yyyy", example = "11-01-2025")
         LocalDate deregistrationDate,
         Long categoryId,
         String imageUrl
